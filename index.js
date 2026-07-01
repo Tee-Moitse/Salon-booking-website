0784e141fcba850caac7de0d41ef983e6c6dc4f1
Tetlanyo Moitse <tetlanyomoitse1@gmail.com>
2026-07-01 21:17:35 +0200

    Cleaned up code and added comment to the index js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+//UI FILE
+
+//PAGE NAVIGATION -> FROM MAIN PAGE TO THE DEPOSITS PAGE
+
 document.getElementById("pay-btn").onclick = function(){
     window.location.href = "payment.html";
 
-
 }
+
